03409ef44e0eab27eabf27a264b041e8d93c53ac
Thyemi de Mello <104704367+thyemimello@users.noreply.github.com>
2025-11-24 22:20:11 -0300

    Add server/storage.ts

---
+++ server/storage.ts
@@ -0,0 +1,34 @@
+import { type ContactSubmission, type InsertContact } from "@shared/schema";
+import { randomUUID } from "crypto";
+
+export interface IStorage {
+  createContactSubmission(contact: InsertContact): Promise<ContactSubmission>;
+  getAllContactSubmissions(): Promise<ContactSubmission[]>;
+}
+
+export class MemStorage implements IStorage {
+  private contacts: Map<string, ContactSubmission>;
+
+  constructor() {
+    this.contacts = new Map();
+  }
+
+  async createContactSubmission(insertContact: InsertContact): Promise<ContactSubmission> {
+    const id = randomUUID();
+    const contact: ContactSubmission = { 
+      ...insertContact, 
+      id,
+      submittedAt: new Date()
+    };
+    this.contacts.set(id, contact);
+    return contact;
+  }
+
+  async getAllContactSubmissions(): Promise<ContactSubmission[]> {
+    return Array.from(this.contacts.values()).sort(
+      (a, b) => b.submittedAt.getTime() - a.submittedAt.getTime()
+    );
+  }
+}
+
+export const storage = new MemStorage();
